d4d9ea031b2fc13c0baefdad497007e00774c109
Yousof <yousof.hosseinpour@gmail.com>
2023-08-04 22:04:23 +0330

    init

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using DelegateExample;
+
+WorkPerformedHandler delPerformed = new WorkPerformedHandler(Worker_WorkPerformed);
+WorkCompletedHandler delConpleted = new WorkCompletedHandler(Worker_WorkCompleted);
+
+Worker worker = new Worker();
+worker.DoWork(5, "Generating Report", delPerformed, delConpleted);
+Console.ReadKey();
+
+static void Worker_WorkPerformed(int hours, string workType)
+{
+    Console.WriteLine($"{hours} Hours compelted for {workType}");
+}
+
+static void Worker_WorkCompleted(string workType)
+{
+    Console.WriteLine($"{workType} work compplted");
+}
---
+++ DelegateExample/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    public delegate void WorkPerformedHandler(int hours, string workType);
+    public delegate void WorkCompletedHandler(string workType);
+
+    public class Worker
+    {
+        public void DoWork(int hours, string workType, WorkPerformedHandler delPerformed, WorkCompletedHandler delCompleted)
+        {
+            delPerformed = null;
+            delCompleted = null;
+            // Do Work here and notify the customer that work has been performed
+            for(int i = 0; i < hours; i++)
+            {
+                // Do Some Processing
+                Thread.Sleep(1000);
+                //Notify how much works have been done
+                delPerformed(i + 1, workType);
+            }
+            delCompleted(workType);
+
+        }
+    }
+}
